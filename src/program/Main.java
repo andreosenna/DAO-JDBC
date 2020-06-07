dc2d8452f56961e2a1cec243f6a70aed368558cc
andreosenna <andreosenna@gmail.com>
2020-06-07 11:15:02 -0300

    teste de conexão

--- src/program/Main.java
+++ src/program/Main.java
@@ -1,11 +1,38 @@
 package program;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import db.DB;
+import db.DBException;
+
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException {
 
+		Connection conn = DB.getConnection();
+		Statement st = null;
+		ResultSet rs = null;
+		try {
+		st = (Statement) conn.createStatement();
+		rs = st.executeQuery("Select * from department");
 		
-
+		while(rs.next()) {
+			System.out.println(rs.getInt(1)+ " - "+ rs.getString(2));
+						}
+			}
+		catch(SQLException e) {
+			throw new DBException(e.getMessage());
+		
+		}
+		
+		
+		
+		DB.closeResultSet(rs);
+		DB.closeStatement(st);
+		DB.closeConnection();
 	}
 
 }
